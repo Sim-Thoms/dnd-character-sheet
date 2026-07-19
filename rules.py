19a01926fdf828abb6d433ba700214704a7c4d00
Simon <simon.th00mas@gmail.com>
2026-07-18 22:14:01 -0400

    Add rules.py and tests/test_rules.py

---
+++ rules.py
@@ -0,0 +1,2 @@
+def ability_modifier(score: int) -> int:
+    return (score - 10) // 2
---
+++ tests/test_rules.py
@@ -0,0 +1,5 @@
+from rules import ability_modifier
+
+
+def test_ability_modifier():
+    assert ability_modifier(12) == 1
