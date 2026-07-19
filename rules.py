99c7a98c4559736ad68f48eb25206e384a3129b8
Simon <simon.th00mas@gmail.com>
2026-07-19 13:40:05 -0400

    Add functions proficiency_bonus() and test_proficiency_bonus()

--- rules.py
+++ rules.py
@@ -1,2 +1,6 @@
 def ability_modifier(score: int) -> int:
     return (score - 10) // 2
+
+def proficiency_bonus(level: int) -> int:
+    return 2 + (level - 1) // 4
+
--- tests/test_rules.py
+++ tests/test_rules.py
@@ -1,5 +1,13 @@
 from rules import ability_modifier
+from rules import proficiency_bonus
+
 
 
 def test_ability_modifier():
     assert ability_modifier(12) == 1
+
+def test_proficiency_bonus():
+    assert proficiency_bonus(3) == 2
+
+
+
