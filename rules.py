9651b3b65b7def488c1a27f96d1d2142ebad1b6a
Simon <simon.th00mas@gmail.com>
2026-07-19 14:02:17 -0400

    Simplify function skill_bonus()

--- rules.py
+++ rules.py
@@ -5,11 +5,9 @@ def proficiency_bonus(level: int) -> int:
     return 2 + (level - 1) // 4
 
 def skill_bonus(score: int, prof_bonus: int, proficient: bool = False, expertise: bool = False):
-    if proficient == False and expertise == False:
-        skill_bonus = ability_modifier(score)
-    elif proficient == True and expertise == False: 
-        skill_bonus = ability_modifier(score) + prof_bonus
-    else:
-        skill_bonus = ability_modifier(score) + (2 * prof_bonus)
-    return skill_bonus
-
+    if expertise:
+        return ability_modifier(score) + (2 * prof_bonus)
+    if proficient:
+        return ability_modifier(score) + prof_bonus
+    return ability_modifier(score)
+ 
